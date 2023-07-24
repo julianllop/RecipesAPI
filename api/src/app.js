61ff1f811101bdad455c614b12a83cd42ba43faf
Julian Llop <juliianllop@gmail.com>
2023-07-24 20:07:43 -0300

    new url

--- api/src/app.js
+++ api/src/app.js
@@ -19,7 +19,7 @@ server.use(morgan("dev"));
 server.use((req, res, next) => {
     res.header(
         "Access-Control-Allow-Origin",
-        "https://recipesapi-front.onrender.com"
+        "https://recipes-api-jl.onrender.com/"
     ); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Credentials", "true");
     res.header(
