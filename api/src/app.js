43ab2b3ceb02e3484eab052b190c9f720a2d7f08
Julian Llop <juliianllop@gmail.com>
2023-08-02 10:21:01 -0300

    cors

--- api/src/app.js
+++ api/src/app.js
@@ -17,10 +17,11 @@ server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
 server.use((req, res, next) => {
-    res.header(
-        "Access-Control-Allow-Origin",
-        "https://recipes-api-jl.onrender.com"
-    ); // update to match the domain you will make the request from
+    const allowedOrigins = ["https://recipes-api-jl.onrender.com"];
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        res.header("Access-Control-Allow-Origin", origin);
+    }
     res.header("Access-Control-Allow-Credentials", "true");
     res.header(
         "Access-Control-Allow-Headers",
